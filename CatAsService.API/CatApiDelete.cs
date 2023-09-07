957b8be65a68156c0d046bc5f6e7c08dfea4aae2
DouglasRbto <109567171+DouglasRbto@users.noreply.github.com>
2023-09-07 13:39:10 -0300

    Update CatApiDelete.cs

--- CatAsService.API/CatApiDelete.cs
+++ CatAsService.API/CatApiDelete.cs
@@ -17,7 +17,7 @@ namespace CatAsService.API
         {
             Client = new RestClient($"https://api.thecatapi.com/v1/favourites/{catId}");
             Request = new RestRequest("", Method.Delete);
-            Request.AddHeader("x-api-key", "live_Jp4ur3Aj7KMPbvovjYONN5rEoMbBb2QvfHwqKevAQj3GhDYqZrIaiXTaaqXOBIIe");
+            Request.AddHeader("x-api-key", "API KEY);
             Response = Client.ExecuteAsync(Request).GetAwaiter().GetResult();
         }
     }
